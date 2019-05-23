c13fc66c4d550afee77becfeef9066741459adf4
Rahul Sai <rahulsaipratap@gmail.com>
2019-05-23 08:01:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./Components/Contacts/Contacts";
 import Header from "./Components/Layout/Header";
 import About from "./Components/Pages/About";
